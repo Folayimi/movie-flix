568d3ab824fe84c261f40fdb9bdc64dea816df76
folayimi <ridwanfolayimi@gmail.com>
2025-09-17 02:21:16 +0100

    added changes to profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -4,16 +4,26 @@ import * as Application from "expo-application";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
 import * as DocumentPicker from "expo-document-picker";
-import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
 import { Image, Platform, Text, TouchableOpacity, View } from "react-native";
-import { CameraIcon } from "react-native-heroicons/outline";
+import { CameraIcon, ChevronLeftIcon } from "react-native-heroicons/outline";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
 
-const useDeviceInfo = () => {
-  const [data, setData] = useState({});
+const Profile = () => {
+  const router = useRouter();
+  const [deviceInfo, setDeviceInfo] = useState<any>({});
+  const [userData, setUserData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    prof_image: "",
+  });
+
+  // Load device info and profile data
   useEffect(() => {
-    const loadData = async () => {
+    const getProfile = async () => {
       const info = {
         device: {
           name: Device.deviceName,
@@ -27,66 +37,99 @@ const useDeviceInfo = () => {
             Platform.OS === "ios"
               ? await Application.getIosIdForVendorAsync()
               : Platform.OS === "android"
-                ? await Application.getAndroidId()
-                : null,
+              ? await Application.getAndroidId()
+              : null,
         },
       };
-      setData(info);
+      setDeviceInfo(info);
+      console.log("Device Info:", info);
+      AsyncStorage.clear();
+      const profileString = await AsyncStorage.getItem("userData");
+      const profileData = profileString ? JSON.parse(profileString) : {};
+      setUserData((prev) => ({ ...prev, ...profileData }));
     };
-    loadData();
+
+    getProfile();
   }, []);
 
-  return data;
-};
-
-const Profile = async () => {
-  const data = await AsyncStorage.getItem("userData")  
-  const profileData:any = data ? JSON.parse(data) : {}
-  const [userData, setUserData] = useState({
-    first_name: profileData || "",
-    last_name: "",
-    email: "",
-    prof_image: "",
-  });
-  const deviceInfo = useDeviceInfo();
-  console.log(deviceInfo);
+  // Pick file
   const pickDoc = async () => {
-    const result = await DocumentPicker.getDocumentAsync({ type: "*/*" });
-    if (result.assets && result.assets.length > 0) {
-      console.log("Picked:", result.assets[0]);
-      setUserData({ ...userData, prof_image: result.assets[0].uri });
+    try {
+      const result = await DocumentPicker.getDocumentAsync({ type: "*/*" });
+      if (result.assets && result.assets.length > 0) {
+        console.log("Picked:", result.assets[0]);
+        const newUserData = { ...userData, prof_image: result.assets[0].uri };
+        setUserData(newUserData);
+        console.log(newUserData);
+        await AsyncStorage.setItem("userData", JSON.stringify(newUserData));
+      }
+    } catch (error) {
+      console.error("An error occurred while selecting file:", error);
     }
   };
+
   return (
-    <SafeAreaView style={tw`bg-blue-950 flex-1 px-10`}>
+    <SafeAreaView style={tw`bg-blue-950 flex-1 px-[10px]`}>
       <View style={tw`flex justify-start items-start flex-1 flex-col gap-5`}>
-        <View
-          style={tw`bg-purple-800 w-[40px] h-[40px] mb-8 rounded-full justify-center items-center`}
-        >
-          <Image
-            source={icons.arrow}
-            style={tw`w-5 h-5 mr-1 mt-0.5 rotate-180`}
-            tintColor="#fff"
-          />
+        <View style={tw`w-full flex-row items-center justify-between`}>
+          <TouchableOpacity
+            style={tw`bg-purple-800 w-[40px] h-[40px] mb-5 mt-2 rounded-full justify-center items-center`}
+            onPress={() => {
+              router.back();
+            }}
+          >
+            <ChevronLeftIcon size={25} color="white" />
+          </TouchableOpacity>
+
+          <Text style={tw`text-sm mr-[40%] text-center text-white font-bold`}>
+            {deviceInfo?.device?.name}
+          </Text>
         </View>
+
+        {/* Profile Image */}
         <View style={tw`w-full p-[10px] flex items-center justify-center`}>
           <View style={tw`relative`}>
             <Image
               source={
                 userData.prof_image ? { uri: userData.prof_image } : icons.empty
               }
-              style={tw`size-50 rounded-full`}
-              tintColor="#fff"
+              style={tw`w-50 h-50 rounded-full`}
+              resizeMode="cover"
             />
             <TouchableOpacity
-              style={tw`absolute bottom-[20px] right-[40px] z-20`}
+              style={tw`absolute bottom-[10px] right-[15px] z-20 bg-purple-800 rounded-full p-[10px]`}
               onPress={pickDoc}
             >
-              <CameraIcon size={40} color="purple" />
+              <CameraIcon size={20} color="white" />
             </TouchableOpacity>
           </View>
         </View>
-        <Text style={tw`text-gray-500 text-base`}>Profile</Text>
+
+        <View style={tw`w-full flex-col gap-[15px] px-[10px]`}>
+          <Text style={tw`text-sm font-bold text-white`}>Device Info</Text>
+          <View
+            style={tw`w-full flex-col gap-[20px] bg-blue-800 rounded-[10px] p-[20px]`}
+          >
+            <View style={tw`w-full flex-row justify-between items-center`}>
+              <Text style={tw`text-sm font-bold text-white`}>Model</Text>
+              <Text style={tw`text-sm font-bold text-white`}>
+                {deviceInfo?.device?.model}
+              </Text>
+            </View>
+            <View style={tw`w-full flex-row justify-between items-center`}>
+              <Text style={tw`text-sm font-bold text-white`}>OS</Text>
+              <Text style={tw`text-sm font-bold text-white`}>
+                {deviceInfo?.device?.os}
+              </Text>
+            </View>
+            <View style={tw`w-full flex-row justify-between items-center`}>
+              <Text style={tw`text-sm font-bold text-white`}>DeviceId</Text>
+              <Text style={tw`text-sm font-bold text-white`}>
+                {deviceInfo?.Identifiers?.androidId}
+              </Text>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
